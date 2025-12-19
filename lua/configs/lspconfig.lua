ae6bc54854ee3eb7c35b6847cee1977c40e7824d
Nick Pashkov <nikita.pashkov@humanapi.co>
2025-12-19 14:04:45 -0600

    custom config

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,6 +1,19 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls" }
+local servers = { "html", "cssls", "ts_ls", "eslint" }
 vim.lsp.enable(servers)
+
+local base_on_attach = vim.lsp.config.eslint.on_attach
+vim.lsp.config("eslint", {
+  on_attach = function(client, bufnr)
+    if not base_on_attach then return end
+
+    base_on_attach(client, bufnr)
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      buffer = bufnr,
+      command = "LspEslintFixAll",
+    })
+  end,
+})
 
 -- read :h vim.lsp.config for changing options of lsp servers 
